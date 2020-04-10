a26a710e0f0730da751f3bd3d25069b580110e80
William Pratt <willpratt78@gmail.com>
2020-04-10 11:19:02 -0700

    add if statment

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // Business Logic for Pizza
 function Pizza() {
-  this.size = 0
-  this.topping = []
+  this.size = 0;
+  this.topping = 0;
 }
 
 Pizza.prototype.addTopping = function(topChoice) {
   if((topChoice).checked) {
-    this.topping.push(topChoice.val())
+    this.topping.push.parseInt((topChoice.val()))
   }
 }
 
@@ -16,13 +16,29 @@ Pizza.prototype.totalCost = function() {
 
 Pizza.prototype.pieSize = function(sizeChoice) {
   if ((sizeChoice).checked) {
-    this.size.push(sizeChoice.val())
+    console.log(sizeChoice)
+    this.size.push(parseInt((sizeChoice.val())))
   }
 }
 
+var sm = document.getElementById("sm")
+var md = document.getElementById("md")
+var lg = document.getElementById("lg")
+var xl = document.getElementById("xl")
 
 
 // UI Logic
-$(document).ready(function() {
+$(document).ready(function(){
+  $('form#pizza-price').submit(function(event){
+    event.preventDefault()
 
-})
+    
+    if (("#sm".checked) === true)
+    totalCost(pieSize)
+    console.log(pieSize)
+    })
+    
+    
+  })
+
+
